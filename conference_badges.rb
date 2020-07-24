657d3d32e9223dcd3bc739d95399ca22f2e33782
Karim Alomari <karim.alomari@gmail.com>
2020-07-24 19:33:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,18 +3,19 @@ def badge_maker(name)
 end
 
 
-def batch_badge_creator(speakers)
+def batch_badge_creator(attendees)
   batch_array = []
-  speakers.each do |name|
+  attendees.each do |name|
     batch_array << badge_maker(name)
   end
   batch_array
 end
 
 
-def assign_rooms(speakers)
+def assign_rooms(attendees)
   room_assignments = []
-  speakers.each_with_index do |name, room|
+  attendees.each_with_index do |name, room|
     room_assignments << "Hello, #{name}! You'll be assigned to room #{room + 1}!"
   end
+  room_assignments
 end
