747c2f3af90c5b655909c43c1f5e5bcbae5638b1
Karim Alomari <karim.alomari@gmail.com>
2020-07-24 19:16:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,4 @@
-def batch_badge_creator(name)
+def conference_badges(name)
   speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   speakers.each do |name|
   puts "Hello, my name is #{name}"
